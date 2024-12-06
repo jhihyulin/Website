a7decba21cbec858ec79c3a0e00fdf98ca62b119
jhih_yu <74854953+jhihyulin@users.noreply.github.com>
2024-12-06 20:04:50 +0800

    refactor: home email

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
 "use client";
 
 import { Button } from "@nextui-org/react";
+import { useDisclosure } from "@nextui-org/react";
+import { Modal } from "@nextui-org/react";
+import { ModalContent } from "@nextui-org/react";
+import { ModalHeader } from "@nextui-org/react";
+import { ModalBody } from "@nextui-org/react";
+import { ModalFooter } from "@nextui-org/react";
 import { Avatar } from "@nextui-org/avatar";
+import { Snippet } from "@nextui-org/snippet";
 import { FaLocationDot } from "react-icons/fa6";
 import { FaGithub } from "react-icons/fa6";
 import { FaInstagram } from "react-icons/fa6";
@@ -9,6 +16,9 @@ import { FaDiscord } from "react-icons/fa6";
 import { FaTelegram } from "react-icons/fa6";
 import { FaEnvelope } from "react-icons/fa6";
 import { FaUniversity } from "react-icons/fa";
+import { FaArrowRight } from "react-icons/fa6";
+import { FaCopy } from "react-icons/fa6";
+import React from "react";
 
 const LocationLinks = [
   {
@@ -43,14 +53,11 @@ const mediaLinks = [
     link: "https://t.me/jhihyulin",
     icon: <FaTelegram size={24} />,
   },
-  {
-    name: "jy@saget.me",
-    link: "mailto:jy@saget.me",
-    icon: <FaEnvelope size={24} />,
-  },
 ];
 
 export default function Home() {
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-4 row-start-2 items-center sm:items-start">
@@ -96,7 +103,7 @@ export default function Home() {
         <div className="flex flex-wrap gap-4 items-center justify-center">
           {mediaLinks.map((item, index) => (
             <Button
-              isIconOnly={item.name ? false : true}
+              isIconOnly
               key={index}
               startContent={item.icon}
               radius="full"
@@ -104,10 +111,55 @@ export default function Home() {
               onPress={() => {
                 window.open(item.link);
               }}
-            >
-              {item.name ? item.name : ""}
-            </Button>
+            />
           ))}
+          <Button
+            isIconOnly
+            startContent={<FaEnvelope size={24} />}
+            radius="full"
+            variant="faded"
+            onPress={onOpen}
+          />
+          <Modal
+            isOpen={isOpen}
+            onOpenChange={onOpenChange}
+            backdrop="blur"
+            classNames={{
+              base: `${
+                typeof window !== "undefined" &&
+                window.matchMedia("(prefers-color-scheme: dark)").matches
+                  ? "dark"
+                  : ""
+              }`,
+            }}
+          >
+            <ModalContent>
+              {(onClose) => (
+                <>
+                  <ModalHeader className="flex flex-col gap-1">
+                    電子郵件
+                  </ModalHeader>
+                  <ModalBody>
+                    <Snippet hideSymbol size="lg">
+                      jy@saget.me
+                    </Snippet>
+                  </ModalBody>
+                  <ModalFooter>
+                    <Button
+                      startContent={<FaArrowRight />}
+                      radius="full"
+                      variant="faded"
+                      onPress={() => {
+                        window.open("mailto:jy@saget.me");
+                      }}
+                    >
+                      傳送電子郵件
+                    </Button>
+                  </ModalFooter>
+                </>
+              )}
+            </ModalContent>
+          </Modal>
         </div>
       </main>
     </div>
